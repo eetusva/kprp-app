19991815ccfbf698ef0e7e6c59550d7b90113e9a
eetusva <66327110+eetusva@users.noreply.github.com>
2025-05-16 02:28:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/kprp-app/', // Updated base path
+  base: '/kprp-app/', // Updated base path to match GitHub Pages repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
