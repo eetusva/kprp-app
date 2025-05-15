3897a24571ccd005ffb827b4711ce513a66e328d
eetusva <66327110+eetusva@users.noreply.github.com>
2025-05-16 02:25:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/modern-message-viewer/',
+  base: '/kprp-app/', // Updated base path
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
